e4c4e5e938fb807ddcbe38dd87037f575da344d4
JeongUpGi <dkdfl1201@gmail.com>
2023-04-17 12:01:42 +0900

    Add Icon & Fix Pallete
    <짧은 요약> : 병원등록하기 홈화면 아이콘 에셋 추가, 주요 색상 변경

--- src/constants/Palette.js
+++ src/constants/Palette.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Colors = {
   patientColor: 'rgb(136,95,255)', // userClass = normalUser에 관한 Color #885FFF
   hospitalOwnerColor: 'rgb(66,153,174 )', // userClass = hospitalOwner 관한 Color
+  heartColor: 'rgb(245,56,116 )', // 건강기록 관련 Color
   alertColor: 'rgb(226,26,58)',
   white: 'rgb(255,255,255)',
   black: 'rgb(0, 0 ,0)',
@@ -13,7 +14,7 @@ const Colors = {
   gray5: 'rgb(200,200,210)', //C8C8D2
   gray6: 'rgb(222,223,230)', //DEDFE6
   gray7: 'rgb(226,226,236)', //E2E2EC
-  gray8: 'rgb(251,251,251)', //FBFBFB
+  gray8: 'rgb(249,249,249)', //FBFBFB
 };
 
 export const mainTheme = {
