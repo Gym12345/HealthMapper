b593d3b804dd4c5367fffc19363c40ef1b9e24bd
JeongUpGi <dkdfl1201@gmail.com>
2023-05-09 02:54:08 +0900

    Dev HospitalInfoScreen UI
    <짧은 요약> : 내 병원 정보 스크린 UI,UX 구현 (각 버튼 기능 별 Redux함수 추가 필요)

---
+++ src/components/HospiltalOwner/MyHospitalInfo.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import {RFValue} from 'react-native-responsive-fontsize';
+import Icons from '../../aseets/Hospital/Icons';
+
+const MyHospitalInfo = props => {
+  return (
+    <Wrapper>
+      <RowDirectionButtonWrapper
+        activeOpacity={0.5}
+        onPress={props.onCheckHospital}>
+        <Icons.myHospital />
+        <ButtonTitle>내 병원 조회</ButtonTitle>
+      </RowDirectionButtonWrapper>
+      <RowDirectionButtonWrapper
+        activeOpacity={0.5}
+        onPress={props.onUpdateHospital}>
+        <Icons.hospitalUpdate />
+        <ButtonTitle>내 병원정보 수정</ButtonTitle>
+      </RowDirectionButtonWrapper>
+      <RowDirectionButtonWrapper
+        activeOpacity={0.5}
+        onPress={props.onCheckReview}>
+        <Icons.myHospitalReview />
+        <ButtonTitle>병원 리뷰 조회</ButtonTitle>
+      </RowDirectionButtonWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.View`
+  margin-horizontal: 30px;
+`;
+const RowDirectionButtonWrapper = styled.TouchableOpacity`
+  margin-top: 30px;
+  flex-direction: row;
+  align-items: center;
+`;
+const ButtonTitle = styled.Text`
+  font-size: ${RFValue(15)}px;
+  color: ${props => props.theme.colors.black};
+  margin-left: 20px;
+`;
+
+export default MyHospitalInfo;
--- src/screens/HospitalOwner/HospitalInformation/HospitalInfoScreen.js
+++ src/screens/HospitalOwner/HospitalInformation/HospitalInfoScreen.js
@@ -1,17 +1,23 @@
 import React, {useCallback} from 'react';
-import {Alert} from 'react-native';
+import {Alert, Dimensions} from 'react-native';
+
 import {useSelector, useDispatch} from 'react-redux';
+import {logout} from '../../../store/slices/authSlice';
+
+import {RFValue} from 'react-native-responsive-fontsize';
 
 import HeaderBar from '../../../components/Global/HeaderBar';
 import styled from 'styled-components';
 import Icons from '../../../aseets/Global/Icons';
 
-import {logout} from '../../../store/slices/authSlice';
+import MyHospitalInfo from '../../../components/HospiltalOwner/MyHospitalInfo';
 
+const {height} = Dimensions.get('window');
 const UserInfoScreen = props => {
   const userId = useSelector(state => state.auth.userId);
   const dispatch = useDispatch();
 
+  //로그아웃 핸들러
   const logoutHandler = useCallback(async () => {
     try {
       await dispatch(logout());
@@ -37,8 +43,19 @@ const UserInfoScreen = props => {
         leadingIcon={<Icons.arrowBack />}
         centerTitle="병원 정보"
       />
-      <Title>현재 입력하신 계정은 {userId} 입니다.</Title>
-      <ButtonWrapper onPress={logoutHandler}>
+      <UserWrapper>
+        <Icons.hospitalUserProfile />
+        <UserIdText>
+          <HighlightText>{userId}</HighlightText>님 환영합니다
+        </UserIdText>
+      </UserWrapper>
+      <DivisionLine />
+      <MyHospitalInfo
+        onCheckHospital={() => {}}
+        onUpdateHospital={() => {}}
+        onCheckReview={() => {}}
+      />
+      <ButtonWrapper activeOpacity={0.5} onPress={logoutHandler}>
         <LogoutButtonText>로그아웃</LogoutButtonText>
       </ButtonWrapper>
     </Container>
@@ -48,16 +65,39 @@ const UserInfoScreen = props => {
 const Container = styled.SafeAreaView`
   background-color: ${props => props.theme.colors.white};
 `;
-const Title = styled.Text``;
-const ButtonWrapper = styled.TouchableOpacity`
-  margin-top: 50px;
+const UserWrapper = styled.View`
+  margin-top: ${height / 30}px;
+  flex-direction: row;
+  padding: 30px;
+  align-items: center;
+`;
+const UserIdText = styled.Text`
+  text-align: center;
+  color: ${props => props.theme.colors.black};
+  margin-left: 20px;
+  font-size: ${RFValue(13)}px;
+`;
+const HighlightText = styled.Text`
+  font-size: ${RFValue(20)}px;
+  font-weight: bold;
+`;
+const DivisionLine = styled.View`
+  background-color: ${props => props.theme.colors.gray7};
   align-self: center;
-  background-color: ${props => props.theme.colors.patientColor};
+  width: 90%;
+  height: 1px;
+`;
+const ButtonWrapper = styled.TouchableOpacity`
+  margin-top: 30px;
+  align-self: center;
+  align-items: center;
+  background-color: ${props => props.theme.colors.hospitalOwnerColor};
   padding: 15px;
+  width: 90%;
   border-radius: 15px;
 `;
 const LogoutButtonText = styled.Text`
-  font-size: 30px;
+  font-size: ${RFValue(20)}px;
   color: ${props => props.theme.colors.white};
 `;
 
