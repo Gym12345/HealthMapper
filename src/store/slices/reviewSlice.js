4f0e5a7431a4029517b2ecfb99e54b7dc9c12b3f
JeongUpGi <dkdfl1201@gmail.com>
2023-05-20 15:51:24 +0900

    Dev checkMyReview_normalUser action _Redux
    <짧은 요약> : 일반사용자가 등록한 자신의 리뷰 조회 액션 함수 구현

--- src/store/slices/reviewSlice.js
+++ src/store/slices/reviewSlice.js
@@ -55,11 +55,39 @@ export const getReview = createAsyncThunk(
   },
 );
 
+// userInfoScreen에서 쓰이는 자신이 작성한 리뷰 조회 컨트롤러
+export const checkMyReview_NormalUSer = createAsyncThunk(
+  'review/checkMyReview_NormalUSer',
+  async ({userId}) => {
+    const response = await fetch(
+      `http://localhost:8090/Health/Health1/ShowMyReviewNormalControllerForJson`,
+      {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({userId}),
+      },
+    );
+
+    const resData = await response.json();
+    console.log(resData);
+
+    if (response.ok) {
+      console.log('내 리뷰조회 성공');
+      return resData;
+    } else {
+      console.log('내 리뷰조회 실패');
+      throw new Error('네트워크 요청 실패');
+    }
+  },
+);
+
 initialState = {
   error: null,
   isReviewRegistered: false,
   isReviesGetted: false,
+  isCheckedMyReview: false,
   reviewArr: [], //리뷰관련 정보 state
+  myReviewArr: [], //내리뷰 관련 정보 state
 };
 
 export const reviewSlice = createSlice({
@@ -68,6 +96,7 @@ export const reviewSlice = createSlice({
   reducers: {},
   extraReducers: builder =>
     builder
+      //리뷰 등록_reviewRegistScreen
       .addCase(reviewRegist.pending, state => {
         state.error = null;
         state.isReviewRegistered = false;
@@ -80,6 +109,7 @@ export const reviewSlice = createSlice({
         state.error = action.error.message;
         state.isReviewRegistered = false;
       })
+      //병원 리뷰 get_hospitalDetailScreen
       .addCase(getReview.pending, state => {
         state.error = null;
       })
@@ -91,6 +121,22 @@ export const reviewSlice = createSlice({
       .addCase(getReview.rejected, (state, action) => {
         state.error = action.error.message;
         state.isReviesGetted = false;
+      })
+      //내 리뷰 조회_userInfoScreen(NorMalUser)
+      .addCase(checkMyReview_NormalUSer.pending, state => {
+        state.error = null;
+        state.isCheckedMyReview = false;
+      })
+      .addCase(checkMyReview_NormalUSer.fulfilled, (state, action) => {
+        state.error = null;
+        state.true = false;
+        state.myReviewArr = action.payload.myReviewArr;
+        console.log('1');
+      })
+      .addCase(checkMyReview_NormalUSer.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.isCheckedMyReview = false;
+        console.log('2');
       }),
 });
 
